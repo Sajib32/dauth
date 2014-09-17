9cfd99da754f5ebc62def80734506b5208d746ec
Sajib32 <sajibcste@gmail.com>
2014-09-17 17:33:00 +0000

    Heroku test mandrill.

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module Dauth
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # Version of your assets, change this if you want to expire all your assets
+    
   end
 end
--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = { 
   :address   => "smtp.mandrillapp.com",
-:port      => 25, # I also tried ports 25 and 587
+:port      => 587, # I also tried ports 25 and 587
 :user_name => "sajibnstu@gmail.com",
-:password  => "nUk56KpXdELbKzQZBHVoYA"
+:password  => "H8bErabNtED4sUqH-p5AqQ"
 }
 
